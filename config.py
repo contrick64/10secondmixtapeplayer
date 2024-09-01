ab921d1c191cd1a97617ebcdc54c1555a7e0a16c
Connor Murphy <connor@murphysites.com>
2024-09-01 18:52:56 -0400

    reconfigure logging

--- config.py
+++ config.py
@@ -2,8 +2,7 @@ import logging
 
 def configure_logging():
     logging.basicConfig(
-        level=logging.DEBUG,
+        level=logging.INFO,
         format='%(asctime)s [%(levelname)s] %(funcName)s() line %(lineno)d: %(message)s',
         datefmt="%Y-%m-%d %H:%M:%S"
     )
-    logging.getLogger('__main__').setLevel(logging.DEBUG)
